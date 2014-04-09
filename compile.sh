0f402cc3c086b95b2b698ca263e6503356da65de
Andrea Mambretti <m4mbr3@gmail.com>
2014-04-08 22:18:00 -0400

    some  changes

--- compile.sh
+++ compile.sh
@@ -9,5 +9,6 @@ $LLVM_ROOT/bin/clang++ -emit-llvm -c $FILECPP -o $FILEBC
 $LLVM_ROOT/bin/llvm-link -o $FILEBC1 $FILEBC  
 $LLVM_ROOT/bin/opt $FILEBC1 -o $FILEBC2 -PrivilegeSeparationOnModule
 $LLVM_ROOT/bin/llc -filetype=obj $FILEBC2 -o $FILEOBJ
-$LLVM_ROOT/bin/clang++ $FILEOBJ -o $FILEEXE 
+$LLVM_ROOT/bin/clang++ -g $FILEOBJ -o $FILEEXE 
 $LLVM_ROOT/bin/clang++ -v -Xlinker "-Tps_link_script.ld"  -Xlinker "--verbose"   $FILEOBJ -o $FILEEXE
+#$LLVM_ROOT/bin/clang++ $FILEOBJ -o $FILEEXE
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-int f2() __attribute__((privilegeSeparation(2)));
-int f() __attribute__((privilegeSeparation(4)));
+#include <unistd.h>
+#include <sys/syscall.h>
+int f1() __attribute__((privilegeSeparation(9)));
+int f2() __attribute__((privilegeSeparation(8)));
+int f3() __attribute__((privilegeSeparation(9)));
+int f4() __attribute__((privilegeSeparation(5)));
+int f5() __attribute__((privilegeSeparation(5)));
+int f6() __attribute__((privilegeSeparation(5)));
+int f7() __attribute__((privilegeSeparation(3)));
+int f8() __attribute__((privilegeSeparation(1)));
+int f9() __attribute__((privilegeSeparation(5)));
 struct ciao2 {
-    std::string name,
-    surname;
-}ciao3 __attribute__((privilegeSeparation(6)));
-int ciao __attribute__((privilegeSeparation(5)));
+    char *name,
+    *surname;
+}ciao3 __attribute__((privilegeSeparation(5)));
+int ciao __attribute__((privilegeSeparation(1)));
 int f()
 {
    // int c __attribute__((privilegeSeparation(3)));
@@ -23,20 +32,65 @@ std::cout << "SADFSADFASDFASDFASDFASDF" << std::endl;
     i = 1 +1;
     return 1;
 }
-int __attribute__((privilegeSeparation(9))) f4() {
-    std::cout << "asdasdasd" << std::endl;
+int f1()
+{
+    std::cout << "Hello World 1!" << std::endl;
+    return 1;
+}
+int f4() {
+    std::cout << "Hello World 4!" << std::endl;
     return 4;
 }
 int f2()
 {
+    std::cout << "Hello World 2!" << std::endl;
+    return 1;
+}
+
+int f3()
+{
+    std::cout << "Hello World 3!" << std::endl;
+    return 1;
+}
+int f5()
+{
+    std::cout << "Hello World 5!" << std::endl;
+    return 1;
+}
+int f6()
+{
+    std::cout << "Hello World 6!" << std::endl;
+    return 1;
+}
+int f7()
+{
     std::cout << "Hello World2!" << std::endl;
     return 1;
 }
-int main(void)
+int f8()
 {
-    f();
-    f4();
+    std::cout << "Hello World2!" << std::endl;
+    return 1;
+}
+int f9()
+{
+    std::cout << "Hello World2!" << std::endl;
+    return 1;
+}
+int __attribute__((privilegeSeparation(9))) main(void)
+{
+    syscall(352, 5);
+    f1();
     f2();
+    f3();
+    f4();
+    f5();
+    syscall(352, 1);
+    f6();
+    f7();
+    f8();
+    f9();
+    f(); 
     std::cout << "Hello World!" << std::endl;
     return 1;
 }
